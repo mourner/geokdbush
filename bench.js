d83fe1965dc05db8888a701f0123a5375c3b298d
Vladimir Agafonkin <agafonkin@gmail.com>
2017-03-17 11:15:04 +0200

    add sphere-knn to benchmark for comparison

--- bench.js
+++ bench.js
@@ -3,17 +3,26 @@
 var cities = require('all-the-cities');
 var kdbush = require('kdbush');
 var geokdbush = require('./');
+var sphereKnn = require('sphere-knn');
 
 console.log(cities.length + ' points');
 
-console.time('index');
+console.time('geokdbush index');
 var index = kdbush(cities, (p) => p.lon, (p) => p.lat);
-console.timeEnd('index');
+console.timeEnd('geokdbush index');
 
-console.time('query');
+console.time('geokdbush query');
 var points = geokdbush.around(index, -119.7051, 34.4363, 1000);
-console.timeEnd('query');
+console.timeEnd('geokdbush query');
 
 console.log('found ' + points.length + ' points');
 
 console.log('first 5: ' + points.slice(0, 5).map(p => p.name).join(', '));
+
+console.time('sphere-knn index');
+var sphereKnnLookup = sphereKnn(cities);
+console.timeEnd('sphere-knn index');
+
+console.time('sphere-knn query');
+sphereKnnLookup(34.4363, -119.7051, 1000);
+console.timeEnd('sphere-knn query');
