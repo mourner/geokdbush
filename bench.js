2e8616d2119c82feee14497c50f59cbf60f8891e
Vladimir Agafonkin <agafonkin@gmail.com>
2017-03-17 13:50:50 +0200

    more benchmarks

--- bench.js
+++ bench.js
@@ -19,6 +19,12 @@ console.log('found ' + points.length + ' points');
 
 console.log('first 5: ' + points.slice(0, 5).map(p => p.name).join(', '));
 
+console.time('geokdbush query for each point');
+for (var i = 0; i < cities.length; i++) {
+    geokdbush.around(index, cities[i].lon, cities[i].lat, 5);
+}
+console.timeEnd('geokdbush query for each point');
+
 console.time('sphere-knn index');
 var sphereKnnLookup = sphereKnn(cities);
 console.timeEnd('sphere-knn index');
@@ -26,3 +32,13 @@ console.timeEnd('sphere-knn index');
 console.time('sphere-knn query');
 sphereKnnLookup(34.4363, -119.7051, 1000);
 console.timeEnd('sphere-knn query');
+
+console.time('sphere-knn query');
+sphereKnnLookup(34.4363, -119.7051, 1000);
+console.timeEnd('sphere-knn query');
+
+console.time('sphere-knn query for each point');
+for (i = 0; i < cities.length; i++) {
+    sphereKnnLookup(cities[i].lat, cities[i].lon, 5);
+}
+console.timeEnd('sphere-knn query for each point');
