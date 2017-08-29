0b6282c3404b9c0653ba2e51777c3cc891bf56b0
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-08-29 16:12:40 +0200

    fix dependency type, compact code
    https://github.com/mourner/geokdbush/pull/5#pullrequestreview-59245927

--- bench/bench-vptree.js
+++ bench/bench-vptree.js
@@ -3,24 +3,9 @@
 var cities = require('all-the-cities');
 var VPTreeFactory = require('vptree');
 
-
-var earthRadius = 6371;
-var rad = Math.PI / 180;
-function greatCircleDist(lng, lat, lng2, lat2, cosLat, sinLat) {
-    var cosLngDelta = Math.cos((lng2 - lng) * rad);
-    return earthRadius * Math.acos(greatCircleDistPart(lat2, cosLat, sinLat, cosLngDelta));
-}
-function greatCircleDistPart(lat, cosLat, sinLat, cosLngDelta) {
-    var d = sinLat * Math.sin(lat * rad) +
-            cosLat * Math.cos(lat * rad) * cosLngDelta;
-    return Math.min(d, 1);
-}
-function distance(p1, p2) {
-    var lng = p1.lon, lat = p1.lat, lng2 = p2.lon, lat2 = p2.lat;
-    return greatCircleDist(lng, lat, lng2, lat2, Math.cos(lat * rad), Math.sin(lat * rad));
-}
-
 console.log('=== vptree.js benchmark ===');
+
+var rad = Math.PI / 180;
 
 var n = cities.length;
 var k = 1000;
@@ -50,3 +35,9 @@ console.timeEnd(`query all ${n}`);
 console.time(`${k} random queries of 1 closest`);
 for (i = 0; i < k; i++) index.search({lon: randomPoints[i].lon, lat: randomPoints[i].lat}, 1);
 console.timeEnd(`${k} random queries of 1 closest`);
+
+function distance(a, b) {
+    var d = Math.sin(a.lat * rad) * Math.sin(b.lat * rad) +
+            Math.cos(a.lat * rad) * Math.cos(b.lat * rad) * Math.cos((a.lon - b.lon) * rad);
+    return 6371 * Math.acos(Math.min(d, 1));
+}
